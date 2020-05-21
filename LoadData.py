aed8a75af25de7d53c9479fa9cdcb2f9ef71b6cd
N1kYan <yannik_phil.frisch@stud.tu-darmstadt.de>
2020-05-21 19:13:34 +0200

    Function to load and preprocess data

---
+++ LoadData.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+
+def load_data():
+    """ Loads data from txt file into pandas frame and performs pre-processing.
+
+    :return: data: Preprocessed pandas data frame
+    """
+
+    # Load data
+    data = pd.read_csv('price.data', sep=",", header=None)
+    data.columns = ['symbol', 'normalized loss', 'wheel base', 'length', 'width', 'height', 'curb weight',
+                    'engine size', 'bore', 'stroke', 'compression ratio', 'horespower', 'peak rpm',
+                    'city mpg', 'highway mpg', 'price']
+    print(f"Loaded {data.shape} data set.")
+
+    # Normalize data
+    data = data.to_numpy()
+    data[:, 1:] = (data[:, 1:] - data[:, :1].mean())/data[:, :1].std()
+    data = pd.DataFrame(data)
+    return data
+
+
+load_data()
+
